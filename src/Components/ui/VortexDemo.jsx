b2b8f3c600b109ac2cf643ebec2a13e67095cf5d
Sandy-8-bit <santhosh.vp.aiml.2022@snsct.org>
2025-05-14 13:17:30 +0530

    Hero padding

--- src/Components/ui/VortexDemo.jsx
+++ src/Components/ui/VortexDemo.jsx
@@ -3,7 +3,7 @@ import { Cover } from "../ui/cover";
 export function VortexDemo() {
   return (
     <div
-      className="w-[calc(100%-4rem)] mx-auto rounded-md  h-[30rem] overflow-hidden">
+      className="w-full mx-auto rounded-md  h-[30rem] overflow-hidden">
       <Vortex
         backgroundColor="black"
         className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full">
